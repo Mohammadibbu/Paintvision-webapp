1bc5b5265c1ec428d617ca9b99a623ac14a404ec
Mohammadibbu <mohammadibbu008@gmail.com>
2024-10-27 23:51:06 +0530

    update

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 
 // Ensure the uploads directory exists
 const ensureUploadsDirectory = () => {
-  const dir = path.join(__dirname, 'uploads');
+  const dir = path.join(__dirname, "uploads");
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
-    console.log('Uploads directory created:', dir);
+    console.log("Uploads directory created:", dir);
   }
 };
 ensureUploadsDirectory();
@@ -19,16 +19,16 @@ ensureUploadsDirectory();
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads'));
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
-  }
+  },
 });
 const upload = multer({ storage: storage });
 
 // Middleware
-app.use(express.static('uploads')); // Serve the uploads directory
+app.use(express.static("uploads")); // Serve the uploads directory
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set view engine to EJS
@@ -57,7 +57,6 @@ app.get("/contacts", (req, res) => {
   res.render("contacts");
 });
 
-<<<<<<< HEAD
 app.get("/interior", (req, res) => {
   res.render("services/interior", { title: "Interior Painting Services" });
 });
@@ -78,20 +77,18 @@ app.get("/color-selection", (req, res) => {
 app.get("/color-mixing", (req, res) => {
   res.render("services/mixing", { title: "Choose Your Colors" });
 });
-=======
+
 // Route to render the upload page
-app.get('/upload', (req, res) => {
-  res.render('upload');
+app.get("/upload", (req, res) => {
+  res.render("upload");
 });
 
-app.post('/upload', upload.single('wallImage'), (req, res) => {
+app.post("/upload", upload.single("wallImage"), (req, res) => {
   const imagePath = `/uploads/${req.file.filename}`;
-  console.log('Uploaded image path:', imagePath); // Log the path for debugging
+  console.log("Uploaded image path:", imagePath); // Log the path for debugging
   res.json({ imagePath });
 });
 
-
->>>>>>> b479a0a (update)
 // Start server
 const PORT = process.env.PORT || 2400;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
